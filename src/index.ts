882b8a0973ad323257f6eeb74448f3027c088660
paulolopestech <phlj.junior@gmail.com>
2024-02-29 20:54:40 -0300

    :sparkles: feat: create prompt client to interact with our app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,50 @@
-type Task = {
-    title: string,
-    description: string,
-    date: Date,
-    type?: string,
-    priority?: string,
-    subtasks?: Task[],
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import readline from 'readline'
+import { promisify } from 'util'
+import { Task, ToDoList } from './Todo'
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const userInput = promisify(reader.question).bind(reader)
+
+async function getUserInput (prompt: string): Promise<any> {
+  return await userInput(prompt)
 }
 
-export class ToDoList {
-  private tasks: Task[] = []
+const taskList = new ToDoList()
 
-  add (task: Task) {
-    this.tasks.push(task)
+async function addNewTask () {
+  const newTask: Task = {
+    title: '',
+    description: '',
+    date: '',
+    type: '',
+    priority: ''
+  }
+  newTask.title = await getUserInput('Digite o título da tarefa: ')
+  newTask.description = await getUserInput('Digite a descrição da tarefa: ')
+  newTask.date = await getUserInput('Digite a data limite da tarefa: ')
+
+  taskList.add(newTask)
+}
+
+async function startTodoList () {
+  const userChoise = await getUserInput('Digite 1 para adicionar uma tarefa ou 0 para sair: ')
+
+  switch (userChoise) {
+    case '0':
+      reader.close()
+      return
+    case '1':
+      await addNewTask()
+      reader.close()
+      return
+    default:
+      reader.close()
   }
 }
+
+startTodoList()
