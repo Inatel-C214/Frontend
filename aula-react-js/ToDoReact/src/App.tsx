aa2697e786827d3c2355ee49870e6fe52b087625
paulolopestech <phlj.junior@gmail.com>
2024-04-02 22:27:07 -0300

    :construction: aula 06

--- aula-react-js/ToDoReact/src/App.tsx
+++ aula-react-js/ToDoReact/src/App.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ToDoList } from "./ToDoList/services/TodoList";
+import { LocalStorageAdapter } from "./ToDoList/repository/LocalStorageAdapter";
+import { Task } from "./ToDoList/models/Task";
 
 type FormData = {
   title: string;
@@ -10,6 +13,8 @@ type FormData = {
 };
 
 function App() {
+  const localStorageAdapter = new LocalStorageAdapter();
+  const tasks = new ToDoList(localStorageAdapter);
   const [formData, setFormData] = useState<FormData>({
     title: "",
     description: "",
@@ -18,12 +23,32 @@ function App() {
     priority: "",
   });
 
+  const [tasksList, setTasksList] = useState<Task[]>([]);
+
   const onChangeInput = (field: string, data: string) => {
     setFormData((currentData) => ({
       ...currentData,
       [field]: data,
     }));
   };
+
+  const createTask = () => {
+    console.log("FMD", formData);
+    tasks.add(formData);
+  };
+
+  const getTasks = () => {
+    const list = tasks.getTasks() as Task[];
+    console.log(list);
+    if (!list) {
+      return;
+    }
+    setTasksList(list);
+  };
+
+  useEffect(() => {
+    getTasks();
+  })
 
   return (
     <>
@@ -32,44 +57,120 @@ function App() {
         <form className="form">
           <div className="input-div">
             <label>Nome</label>
-            <input
-              type="text"
-              onChange={(e) => onChangeInput("title", e.target.value)}
-            ></input>
-          </div>
-
-          <div className="input-div">
-            <label>Descrição</label>
-            <input
-              type="text"
-              onChange={(e) => onChangeInput("description", e.target.value)}
-            ></input>
+            <div
+              style={{
+                width: "10rem",
+              }}
+            >
+              <input
+                style={{
+                  width: "95%",
+                }}
+                type="text"
+                onChange={(e) => onChangeInput("title", e.target.value)}
+              ></input>
+            </div>
           </div>
 
           <div className="input-div">
             <label>Data de entrega</label>
-            <input
-              type="text"
-              onChange={(e) => onChangeInput("targetDate", e.target.value)}
-            ></input>
+            <div
+              style={{
+                width: "10rem",
+              }}
+            >
+              <input
+                style={{
+                  width: "95%",
+                }}
+                type="text"
+                onChange={(e) => onChangeInput("targetDate", e.target.value)}
+              ></input>
+            </div>
           </div>
 
           <div className="input-div">
             <label>Tipo</label>
-            <input
-              type="text"
-              onChange={(e) => onChangeInput("type", e.target.value)}
-            ></input>
+            <div
+              style={{
+                width: "10rem",
+              }}
+            >
+              <input
+                style={{
+                  width: "95%",
+                }}
+                type="text"
+                onChange={(e) => onChangeInput("type", e.target.value)}
+              ></input>
+            </div>
           </div>
 
           <div className="input-div">
             <label>Prioridade</label>
-            <input
-              type="text"
-              onChange={(e) => onChangeInput("priority", e.target.value)}
-            ></input>
+            <div
+              style={{
+                width: "10rem",
+              }}
+            >
+              <select
+                style={{
+                  width: "100%",
+                }}
+                onChange={(e) => onChangeInput("priority", e.target.value)}
+              >
+                <option value="Alta">Alta</option>
+                <option value="Média">Média</option>
+                <option value="Baixa">Baixa</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="input-div">
+            <label>Descrição</label>
+            <div
+              style={{
+                width: "10rem",
+              }}
+            >
+              <textarea
+                rows={10}
+                style={{
+                  resize: "none",
+                  width: "95%",
+                }}
+                onChange={(e) => onChangeInput("description", e.target.value)}
+              ></textarea>
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "right",
+            }}
+            className="input-div"
+          >
+            <button
+              style={{
+                borderRadius: 1,
+                width: "100px",
+              }}
+              onClick={(e) => {
+                e.preventDefault();
+                createTask();
+              }}
+            >
+              Salvar
+            </button>
           </div>
         </form>
+
+        <div>
+          {tasksList.map((task) => (
+            <div>Titulo: {task.title}</div>
+          ))}
+        </div>
       </div>
     </>
   );
